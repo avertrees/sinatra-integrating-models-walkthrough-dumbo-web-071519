5248e93b55aea446cab0af06cc2da33580a7a9ca
Alessandra Vertrees <avertrees447@gmail.com>
2019-08-07 05:59:10 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,14 +1,17 @@
 require_relative 'config/environment'
-require_relative 'models/text_analyzer.rb'
+require_relative "./models/text_analyzer.rb"
 
 class App < Sinatra::Base
   get '/' do
     erb :index
   end
 
-  post '/' do
-    text_from_user = params[:user_text]
+  post '/' do  
 
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
+    @most_used_character = @analyzed_text.most_used_letter.keys[0]
+    @numer_of_times = @analyzed_text.most_used_letter[@most_used_character]
     erb :results
+
   end
 end
